7a323fce9c45de8e5f3203bfb8bc68437e3260a6
ArmishMunir <armish.muneer@gmail.com>
2022-11-12 23:55:06 +0500

    goals and target screen

---
+++ app/src/main/java/com/example/gym/Set_Goals.java
@@ -0,0 +1,14 @@
+package com.example.gym;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Set_Goals extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_goals);
+    }
+}
